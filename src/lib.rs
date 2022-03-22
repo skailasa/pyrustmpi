2c34bb5de3582b4c9bd58c0b6aea732023dcfd34
Srinath Kailasa <srinathkailasa@gmail.com>
2022-03-22 13:15:59 +0000

    Appease clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-use std::mem;
-
 use mpi::traits::*;
-use mpi::environment::Universe;
 use mpi::topology::UserCommunicator;
 
-use mpi::ffi::{MPI_Comm, MPI_Group, MPI_Comm_free};
+use mpi::ffi::{MPI_Comm, MPI_Comm_free};
 
 #[no_mangle]
 pub extern "C" fn sayhello(comm: MPI_Comm) {
-    let mut comm = std::mem::ManuallyDrop::new(unsafe {UserCommunicator::from_raw(comm)}.unwrap());
+    let comm = std::mem::ManuallyDrop::new(unsafe {UserCommunicator::from_raw(comm)}.unwrap());
     let rank = comm.rank();
     println!("Hello from {:?}", rank);
 }
