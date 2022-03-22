7d11e8154619b4facfe6eb40b728f864dc6f9dfb
Srinath Kailasa <srinathkailasa@gmail.com>
2022-03-22 17:23:40 +0000

    Fix example

--- example.py
+++ example.py
@@ -1,26 +1,5 @@
-import os
-
-from cffi import FFI
 from mpi4py import MPI
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-LIBDIR = os.path.join(HERE, "target", "debug")
- 
-ffi = FFI()
-
-if MPI._sizeof(MPI.Comm) == ffi.sizeof('int'):
-    _mpi_comm_t = 'int'
-else:
-    _mpi_comm_t = 'void*'
-
-ffi.cdef("""
-typedef %(_mpi_comm_t)s MPI_Comm;
-void sayhello(MPI_Comm);
-void cleanup(MPI_Comm);
-""" % vars()
-)
-
-lib = ffi.dlopen(os.path.join(LIBDIR,  "libpyrustmpi.so"))
+from pyrustmpi import lib, ffi
 
 comm = MPI.COMM_WORLD
 comm_ptr = MPI._addressof(comm)
